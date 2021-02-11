96685dc9a4446164bb26d9ea0fede5613b9a0b7d
adabawolska <adabawolska@icloud.com>
2021-02-11 17:11:13 +0100

    Initial commit

---
+++ src/test/java/pl/adabawolska/simpleeconomygamespringboot/SimpleEconomyGameSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adabawolska.simpleeconomygamespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEconomyGameSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
